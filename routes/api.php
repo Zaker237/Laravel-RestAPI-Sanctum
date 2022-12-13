03b85eb8b8c246be94ccd520715841af36fcd275
Mboutchouang <mboutchouangalex+github@gmail.com>
2022-12-13 13:29:14 +0100

    fix: search route on Product

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('products', ProductController::class);
-Route::get('products/search/{name}', ProductController::class, 'search');
+Route::get('products/search/{name}', [ProductController::class, 'search']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
